275cd23ad162f4099d694e3b0ef77e5b1e6ec27f
novhack <42614907+novhack@users.noreply.github.com>
2024-10-27 21:57:45 +0100

    Cleanup start view

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -3,7 +3,13 @@
 
 <template>
     <div class="d-flex fill-height align-center justify-center flex-column">
-        <h2>{{ $t('startView.gameTitle') }}</h2>
+        <h2 class="game-title">{{ $t('startView.gameTitle') }}</h2>
         <v-btn to="/game"> {{ $t('startView.playButton') }}</v-btn>
     </div>
 </template>
+
+<style scoped>
+.game-title {
+    padding-bottom: 1vh;
+}
+</style>
