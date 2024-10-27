9ff7e287878e1656d0fef9d6826e6d66e1b09780
novhack <42614907+novhack@users.noreply.github.com>
2024-10-27 23:42:24 +0100

    Increase the size of countdown label

--- src/components/CountdownLabel.vue
+++ src/components/CountdownLabel.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import { useCountdown } from '../composables/useCountdown';
-
 const { countdown } = useCountdown();
-
 </script>
 
 <template>
-    <div class="d-flex flex-row">
+    <div class="countdown-label d-flex flex-row">
         <v-icon icon="mdi-hospital" />
-        <div>{{ countdown }}s</div>
+        <div>{{ countdown }} s</div>
     </div>
 </template>
+
+<style scoped>
+.countdown-label {
+    font-size: 2.5vh;
+}
+</style>
